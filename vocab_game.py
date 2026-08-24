59867eccf40ceb8b160eb322ef28dc609ac15ea3
414-30-ahmee <56787@yupparaj.ac.th>
2026-08-24 14:41:29 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -28,15 +28,12 @@ def reset_game():
 # 📌 ฟังก์ชัน MessageBox (Dialog)
 # ----------------------------------------------------
 @st.dialog("📊 สรุปผลการเล่นเกม")
-def show_result_dialog(ans1 ans2 ans3, ans4):
-
+def show_result_dialog(ans1, ans2):
     st.balloons()
     score = 0
 
     u_ans1 = ans1.strip().lower()
     u_ans2 = ans2.strip().lower()
-    u_ans3 = ans3.strip().lower()
-    u_ans4 = ans4.strip().lower()
 
     # ตรวจข้อ 1
     if u_ans1 == "apple":
@@ -52,20 +49,21 @@ def show_result_dialog(ans1 ans2 ans3, ans4):
     else:
         st.error(f"❌ ข้อ 2: ยังไม่ถูกต้อง (คุณตอบ '{u_ans2}')")
 
-     # ตรวจข้อ 3
-    if u_ans1 == "cherry":
-        st.success("✅ ข้อ 3: ถูกต้อง")
+    # ✏️ [พื้นที่สำหรับนักเรียน]: เพิ่มตรวจข้อ 3, 4 ตรงนี้
+
+    # ตรวจข้อ 3
+    if u_ans1 == "grapes":
+        st.success("✅ ข้อ 1: ถูกต้อง")
         score += 1
     else:
-        st.error(f"❌ ข้อ 3: ยังไม่ถูกต้อง (คุณตอบ '{u_ans1}')")
+        st.error(f"❌ ข้อ 1: ยังไม่ถูกต้อง (คุณตอบ '{u_ans1}')")
 
     # ตรวจข้อ 4
-    if u_ans2 == "banana":
-        st.success("✅ ข้อ 4: ถูกต้อง")
+    if u_ans2 == "cherry":
+        st.success("✅ ข้อ 2: ถูกต้อง")
         score += 1
     else:
-        st.error(f"❌ ข้อ 4: ยังไม่ถูกต้อง (คุณตอบ '{u_ans2}')")
-
+        st.error(f"❌ ข้อ 2: ยังไม่ถูกต้อง (คุณตอบ '{u_ans2}')")
     st.info(f"🏆 ได้คะแนนรวม: {score} คะแนน")
 
     if score == 4:
@@ -100,23 +98,19 @@ ans2 = st.text_input(
     "ข้อ 2: Cats love to eat `f _ s h`. 🐟",
     value=st.session_state.ans2_val,
 )
-ans1 = st.text_input(
-    "ข้อ 1: An `c _ e r _ y`A small red fruit . 🍒",
+ans3 = st.text_input(
+    "ข้อ 1: An `g e  _ p _ s  ` It grows in big bunches.🍇 ",
     value=st.session_state.ans1_val,
 )
-ans1 = st.text_input(
-    "ข้อ 1: An `b a _ a _ a`Monkeys love to eat . 🍌",
-    value=st.session_state.ans1_val,
+ans4 = st.text_input(
+    "ข้อ 2: Cats love to eat `c h e _ _ y`I am a small, round fruit.🍒 ",
+    value=st.session_state.ans2_val,
 )
-
 # อัปเดตค่าล่าสุดเข้าตัวแปร
 st.session_state.ans1_val = ans1
 st.session_state.ans2_val = ans2
-
-st.session_state.ans1_val = ans3
-st.session_state.ans2_val = ans4
-
-
+st.session_state.ans3_val = ans3
+st.session_state.ans4_val = ans4
 
 # 4. ปุ่มส่งคำตอบ
 if "start" in st.session_state and not st.session_state.get("is_ended", False):
@@ -129,7 +123,7 @@ if "start" in st.session_state and not st.session_state.get("is_ended", False):
 
 # 5. แสดง Dialog ผลลัพธ์
 if st.session_state.get("is_ended", False):
-    show_result_dialog(ans1, ans2)
+    show_result_dialog(ans1 ans2 ans3 ans4)
 
 st.divider()
 st.write("นางสาวอะหมี่ หย่าจา เลขที่ 30 ม.4/14")
